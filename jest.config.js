a51c45f11fd8fd11296f38b267efecf1e9956834
itelet <szabo.adam.0725@gmail.com>
2025-04-16 15:50:45 +0200

    fix jest config, add --no-cache to test run script

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["./tests/*"],
+	testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
 };
