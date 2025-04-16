9c29a3086b4989f933ba41e895f20e59f8c0ba8b
itelet <szabo.adam.0725@gmail.com>
2025-04-16 15:49:39 +0200

    implement failing test cases

---
+++ tests/unit/due-date-calculator.test.ts
@@ -0,0 +1,87 @@
+const CalculateDueDate = require("../../src/dueDateCalculator");
+
+describe("due-date-calculator", () => {
+  it("default test case", () => {
+    let submitDate = new Date(2025, 3, 15, 14, 12);
+    let turnaroundTime = 16;
+
+    const expectedOutput = new Date(2025, 3, 17, 14, 12);
+
+    const result = CalculateDueDate({ submitDate, turnaroundTime });
+    expect(result).toEqual(expectedOutput);
+  });
+
+  describe("failing test cases", () => {
+    it("should throw an error if submitDate is not a Date object", () => {
+			const invalidDate = "2025-04-15T14:11:00Z";
+			const turnaroundTime = 16;
+
+			console.log('invalidDate: ', invalidDate);
+
+			expect(() => {
+				CalculateDueDate({ submitDate: invalidDate, turnaroundTime });
+			}).toThrow("Submit date must be a valid Date object");
+		});
+
+    it("should throw an error if submitDate is invalid", () => {
+			const invalidDate = new Date("invalid-date");
+			const turnaroundTime = 16;
+
+			expect(() => {
+				CalculateDueDate({ submitDate: invalidDate, turnaroundTime });
+			}).toThrow("Submit date must be a valid Date object");
+		});
+
+    it("should throw an error if turnaroundTime is not a number", () => {
+			const invalidDate = new Date(2025, 3, 15, 14, 12);
+			const turnaroundTime = "invalid-time";
+
+			expect(() => {
+				CalculateDueDate({ submitDate: invalidDate, turnaroundTime });
+			}).toThrow("Turnaround time must be a positive number");
+		});
+
+    it("should throw an error if turnaroundTime is negative", () => {
+			const invalidDate = new Date(2025, 3, 15, 14, 12);
+			const turnaroundTime = -16;
+
+			expect(() => {
+				CalculateDueDate({ submitDate: invalidDate, turnaroundTime });
+			}).toThrow("Turnaround time must be a positive number");
+		});
+
+    it("should throw an error if submitDate is on a weekend", () => {
+			const invalidDate = new Date(2025, 3, 19, 14, 12);
+			const turnaroundTime = 8;
+
+			expect(() => {
+				CalculateDueDate({ submitDate: invalidDate, turnaroundTime });
+			}).toThrow("Submit date must be during a work day (Monday to Friday)");
+		});
+
+    it("should throw an error if submitDate is outside working hours", () => {
+			const invalidDate = new Date(2025, 3, 15, 18, 12);
+			const turnaroundTime = 8;
+
+			expect(() => {
+				CalculateDueDate({ submitDate: invalidDate, turnaroundTime });
+			}).toThrow("Submit date must be during working hours (9AM to 5PM)");
+		});
+  });
+
+  describe("successful test cases", () => {
+    it("entry + 4 hours", () => {
+
+		});
+
+    it("entry + 8 hours", () => {});
+
+    it("entry + 9 hours", () => {});
+
+    it("entry + 16 hours", () => {});
+  });
+
+  describe("edge cases", () => {
+    it("should handle weekend", () => {});
+  });
+});
